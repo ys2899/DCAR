11dca8e6875e25c373354e52fadc0165aabbf502
Yang Sun <ys46135@Yangs-MacBook-Pro.local>
2020-12-09 11:18:31 +0800

    rec_train.sh to record the training history

---
+++ rec_train.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+if [ -d "./training_history" ]
+then
+    echo "Directory training_history exists."
+else
+    mkdir ./training_history
+fi
+
+
+now=$(date +%Y%m%d%H%M%S)
+
+
+logfile=./training_history/mylogfile_$now
+
+
+mkdir $logfile
+
+
+find -iname '*.py' -exec cp {} $logfile \;
+
+
+nohup python dcARrnn_train.py --config_filename=data/model/dcrnn_la.yaml > $logfile/log.out 2>&1 &
